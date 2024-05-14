d0201f36b86da884e5bb76d60c06e50ac0c3451c
Ahmed Aly <116496822+a7mdli@users.noreply.github.com>
2024-05-14 17:32:45 +0300

    Update thread.c

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -463,6 +463,7 @@ init_thread (struct thread *t, const char *name, int priority)
   strlcpy (t->name, name, sizeof t->name);
   t->stack = (uint8_t *) t + PGSIZE;
   t->priority = priority;
+  t->next_fd = 2;
   t->magic = THREAD_MAGIC;
   old_level = intr_disable ();
   list_push_back (&all_list, &t->allelem);
